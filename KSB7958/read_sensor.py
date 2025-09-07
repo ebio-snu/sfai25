02b7b298133997c43a8f7c266dcf314cbe861baa
JoonYong <croft@farmos.co.kr>
2025-09-07 18:10:39 +0900

    센서 테스트 코드 업데이트

--- KSB7958/read_sensor.py
+++ KSB7958/read_sensor.py
@@ -6,6 +6,7 @@
 # All right reserved.
 #
 
+import time
 import struct
 import json
 from pymodbus.client import ModbusTcpClient
@@ -14,13 +15,51 @@ from ksconstants import STATCODE, CMDCODE, PRIVCODE
 def getobservation(reg1, reg2):
     return struct.unpack('f', struct.pack('HH', reg1, reg2))[0]
 
+def readsensor(client, item):
+    res = client.read_holding_registers(item["start-address"], count=3, device_id=item["unit"])
+    if res.isError():
+        print ("정보를 읽어오는데 실패했습니다.")
+    else:
+        reg = res.registers
+        print (reg)
+        if reg[2] == STATCODE.READY:
+            val = getobservation(reg[0], reg[1])
+            print ("{} 센서의 상태는 정상이고, 관측치는 {} 입니다.".format(item["name"], val))
+        else:
+            print ("{} 센서의 상태가 비정상입니다.".format(item["name"]))
+
+
 # Load configuration
 with open('conf.json', 'r') as f:
     config = json.load(f)
 
+devices = [
+    {"unit": 2, "start-address": 203, "name": "기상대-온도"},
+    {"unit": 2, "start-address": 212, "name": "기상대-습도"},
+    {"unit": 2, "start-address": 218, "name": "기상대-감우"},
+    {"unit": 2, "start-address": 227, "name": "기상대-일사"},
+    {"unit": 2, "start-address": 230, "name": "기상대-풍속"},
+    {"unit": 2, "start-address": 233, "name": "기상대-풍향"},
+    {"unit": 3, "start-address": 203, "name": "내부-온도"},
+    {"unit": 3, "start-address": 212, "name": "내부-습도"},
+    {"unit": 3, "start-address": 239, "name": "내부-이산화탄소"},
+    {"unit": 3, "start-address": 242, "name": "배지-EC"},
+    {"unit": 3, "start-address": 248, "name": "배지-함수율"},
+    {"unit": 3, "start-address": 257, "name": "배지-온도"},
+    {"unit": 5, "start-address": 204, "name": "양액-EC"},
+    {"unit": 5, "start-address": 213, "name": "양액-pH"},
+    {"unit": 5, "start-address": 225, "name": "양액-누적유량"}
+]
+
 client = ModbusTcpClient(config['modbus_ip'], port=config['modbus_port'])
 client.connect()
 
+for dev in devices:
+    readsensor(client, dev)
+    time.sleep(1)
+
+
+"""
 # 2번 유닛아이디 203번지부터 3개의 레지스터를 읽습니다. (외부 온도)
 res = client.read_holding_registers(203, count=3, device_id=2)
 if res.isError():
@@ -47,4 +86,5 @@ else:
     else:
         print ("습도 센서의 상태가 비정상입니다.")
 
+"""
 
